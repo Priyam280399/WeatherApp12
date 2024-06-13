df29f076dc2a3d3198e7bf992463e6fc47f1280d
Priyam280399 <116102211+Priyam280399@users.noreply.github.com>
2024-06-14 00:33:59 +0530

    Add files via upload

---
+++ weatherApp-Reactjs-master/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "12dbefd47af38bee363a3e00362c9815",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
